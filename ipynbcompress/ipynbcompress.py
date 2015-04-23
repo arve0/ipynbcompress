0b3e2dfff8d7cab88e9ae7a4b3a5bf2d10b79811
arve0 <arve.seljebu@gmail.com>
2015-04-23 12:24:27 +0200

    use IPython.nbformat to read/write notebooks, fix jpg->jpeg pil format

--- ipynbcompress/ipynbcompress.py
+++ ipynbcompress/ipynbcompress.py
@@ -1,23 +1,23 @@
 # coding: utf-8
-from json import load, dump
 from base64 import b64decode, b64encode
 from PIL import Image
 from io import BytesIO
 from os import stat
+from IPython.nbformat import read, write
 
 
-def compress(filename, img_width=1024, img_format='jpg', output_filename=None):
+def compress(filename, img_width=1024, img_format='jpeg', output_filename=None):
     """Compress images in IPython notebooks.
 
     Parameters
     ----------
     filename : string
-        Notebook to compress.
+        Notebook to compress. Will take any notebook format.
     img_width : int
         Which width images should be resized to.
     img_format : string
         Which compression to use on the images, valid options are
-        *jpg* and *png*.
+        *png* and *jpeg* (**requires libjpeg**).
     output_filename : string
         If you do not want to overwrite your existing notebook, supply an
         filename for the new compressed notebook.
@@ -29,40 +29,44 @@ def compress(filename, img_width=1024, img_format='jpg', output_filename=None):
     """
     orig_filesize = stat(filename).st_size
 
-    with open(filename) as f:
-        json = load(f)
-        outputs = [cell.get('outputs', []) for cell in j['cells']]
-        # omit empty outputs
-        outputs = [o for o in outputs if len(o)]
-        # flatten
-        outputs = [o for lines in outputs for o in lines]
-        for output in outputs:
-            data = output.get('data', {})
-            if not data:
-                continue
-            keys = data.keys()
-            for key in keys:
-                if 'image' in key:
-                    bytes_img = b64decode(''.join(data[key]))
-                    io_img = BytesIO(bytes_img)
-                    img = Image.open(io_img)
-                    factor = img_width / img.size[0]
-                    if factor < 1:
-                        # only resize large images
-                        new_size = [round(s*factor) for s in img.size]
-                        img = img.resize(new_size)
-                    out = BytesIO()
-                    img.save(out, img_format)
-                    out.seek(0)
-                    b64_img = b64encode(out.read())
-                    del data[key]
-                    data['image/' + img_format] = _chunkstring(b64_img, 77)
+    # compress images
+    nb = read(filename, as_version=4)
+    outputs = [cell.get('outputs', []) for cell in nb['cells']]
+    # omit empty outputs
+    outputs = [o for o in outputs if len(o)]
+    # flatten
+    outputs = [o for lines in outputs for o in lines]
+    for output in outputs:
+        data = output.get('data', {})
+        if not data:
+            continue
+        keys = data.keys()
+        for key in keys:
+            if 'image' in key:
+                string = ''.join(data[key])
+                bytes_img = b64decode(string)
+                io_img = BytesIO(bytes_img)
+                img = Image.open(io_img)
+                factor = float(img_width) / img.size[0]
+                if factor < 1:
+                    # only resize large images
+                    new_size = [int(s*factor+0.5) for s in img.size]
+                    img = img.resize(new_size)
+                out = BytesIO()
+                img.save(out, img_format)
+                out.seek(0)
+                b64_img = b64encode(out.read())
+                del data[key]
+                data['image/' + img_format] = _chunkstring(b64_img, 77)
 
     # save notebook
     if not output_filename:
         output_filename = filename
-    with open(output_filename, 'w') as f:
-        dump(json, f, indent=2, sort_keys=True)
+    try:
+        output_format = nb.metadata.orig_nbformat
+    except AttributeError:
+        output_format = 4
+    write(nb, output_filename, version=output_format)
 
     # calculate bytes saved
     bytes_saved = orig_filesize - stat(output_filename).st_size
