295a8764a4f9b6e459b085df100c8f2c75c1d652
arve0 <arve.seljebu@gmail.com>
2015-04-23 12:25:41 +0200

    test both v3 and v4 notebook formats

--- test/test_compress.py
+++ test/test_compress.py
@@ -7,20 +7,26 @@ from py import path
 
 
 @pytest.fixture
-def notebook(tmpdir):
-    "'notebook.ipynb' in tmpdir. Returns py.path object."
-    n = path.local(__file__).dirpath().join('notebook.ipynb')
-    n.copy(tmpdir)
-    return tmpdir.join('notebook.ipynb')
+def notebooks(tmpdir):
+    "v3 and v4 notebook in tmpdir. Returns list of py.path object."
+    testdir = path.local(__file__).dirpath()
+    n3 = testdir.join('notebook3.ipynb')
+    n4 = testdir.join('notebook4.ipynb')
+    n3.copy(tmpdir)
+    n4.copy(tmpdir)
+    return tmpdir.listdir()
 
 
-def test_stitch(tmpdir, notebook):
+def test_stitch(tmpdir, notebooks):
     "It should compress a notebook."
-    outfile = tmpdir.join('out.ipynb')
-    saved = compress(notebook.strpath, 800, 'jpg', outfile.strpath)
+    for notebook in notebooks:
+        outfile = tmpdir.join('out.ipynb')
+        saved = compress(notebook.strpath, 800, 'jpeg', outfile.strpath)
 
-    # it should not gain in size
-    assert saved > 0
+        # it should not gain in size
+        assert saved > 0
 
-    # output file should exists
-    assert outfile.check()
+        # output file should exists
+        assert outfile.check()
+
+        outfile.remove()
