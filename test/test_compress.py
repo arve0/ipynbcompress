db3c827f4fbfd23a82f19f852ca88ec4abcf5011
arve0 <arve.seljebu@gmail.com>
2015-04-23 14:38:00 +0200

    keep notebooks in own dir - preventing mixing of output/input, validate notebook after write

--- test/test_compress.py
+++ test/test_compress.py
@@ -3,6 +3,7 @@ Tests for `ipynbcompress`.
 """
 import pytest
 from ipynbcompress import *
+from IPython.nbformat import validate, read, NO_CONVERT
 from py import path
 
 
@@ -12,9 +13,10 @@ def notebooks(tmpdir):
     testdir = path.local(__file__).dirpath()
     n3 = testdir.join('notebook3.ipynb')
     n4 = testdir.join('notebook4.ipynb')
-    n3.copy(tmpdir)
-    n4.copy(tmpdir)
-    return tmpdir.listdir()
+    notebooks = tmpdir.join('notebooks').mkdir()
+    n3.copy(notebooks)
+    n4.copy(notebooks)
+    return notebooks.listdir()
 
 
 def test_stitch(tmpdir, notebooks):
@@ -28,5 +30,7 @@ def test_stitch(tmpdir, notebooks):
 
         # output file should exists
         assert outfile.check()
+
+        validate(read(outfile.strpath, NO_CONVERT))
 
         outfile.remove()
